d9633d7a3a040577151ef703805b5166d5ecd6e5
abhiram kosuru <abhiramkosuru967@gmail.com>
2025-09-28 11:15:11 +0530

    Fix: added react-components as normal folder

---
+++ react-components/src/FunctionalComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FunctionalComponent = () => {
+  return (
+    <div style={{ border: "2px solid green", padding: "10px", margin: "10px" }}>
+      <h2>Functional Component</h2>
+      <p>This is a simple Functional Component in React.</p>
+    </div>
+  );
+};
+
+export default FunctionalComponent;
